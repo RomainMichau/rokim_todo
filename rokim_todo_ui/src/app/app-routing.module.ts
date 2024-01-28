a2c2082fcbbf8f8ec3a1f55fb0cec226c5992aac
r.michau <r.michau@criteo.com>
2024-01-28 11:50:10 +0100

    Fix path

--- rokim_todo_ui/src/app/app-routing.module.ts
+++ rokim_todo_ui/src/app/app-routing.module.ts
@@ -5,8 +5,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { TodoListComponent } from './todo-list/todo-list.component';
 
 const routes: Routes = [
-  { path: 'todos', component: TodoListComponent },
-  { path: '', redirectTo: '/todos', pathMatch: 'full' },
+  { path: '', component: TodoListComponent },
 ];
 
 @NgModule({
