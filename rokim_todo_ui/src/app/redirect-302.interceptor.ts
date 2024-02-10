a89957d31871015398d7b7da8316cb95dfd3975b
r.michau <r.michau@criteo.com>
2024-02-10 11:59:54 +0100

    Redirect user when 302

---
+++ rokim_todo_ui/src/app/redirect-302.interceptor.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpResponse,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class RedirectInterceptor implements HttpInterceptor {
+
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(request).pipe(
+        catchError((error: any) => {
+          if (error instanceof HttpErrorResponse && error.status === 302) {
+            // Extract the location header from the response
+            const locationHeader = error.headers.get('Location');
+            if (locationHeader) {
+              // Navigate to the URL specified in the location header
+              window.location.href = locationHeader;
+            } else {
+              console.error('Location header not found in the response');
+            }
+          }
+          return throwError(error);
+        })
+    );
+  }
+}
